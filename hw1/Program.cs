4f1eb228e36543979417babb6201bef67981f28e
Lola <lola@example.com>
2022-09-19 23:50:19 +0300

    task1

--- hw1/Program.cs
+++ hw1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите трехзначное число");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int FirstResult = Number / 10;
+
+int result = FirstResult % 10;
+Console.WriteLine($"Вторая цифра данного трехзначного числа это {result}");
